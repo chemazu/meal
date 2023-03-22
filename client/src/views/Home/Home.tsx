607ab8d8785924839f972361b3247c019067479a
chemazu <chukwuemekachemazu@gmail.com>
2023-03-23 00:09:10 +0100

    meal display item v1 created

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import NavBar from "../../component/Navbar";
+import tasty from "../../images/tasty.svg";
+import nuts from "../../images/nuts.svg";
+import mealImg from "../../images/chicken-rice.jpg";
+
+import "../../styles/home.scss";
 
 export default function Home() {
   return (
@@ -10,7 +15,46 @@ export default function Home() {
           Fresh, healthy, and delicious meals made just for you: experience the
           best in meal prep delivery
         </h2>
+        <p>
+          Nature offers everything you need. Nothing more and nothing less. Just
+          right as it is https://tastybasics.nl/
+        </p>
+        <div className="search-div">
+          <input type="search" placeholder="What are you Looking for" />
+          <button></button>
+        </div>
+        <div className="search-suggestions">
+          <p className="suggestion">Weight Loss</p>
+          <p className="suggestion">Meal Prep</p>
+          <p className="suggestion">Suya Chicken Breast</p>
+          <p className="suggestion">Asun Beef and Chips</p>
+          <img src={tasty} />
+          <img src={nuts} />
+        </div>
       </header>
+      <section className="how-it-works">
+        <h3>How it works</h3>
+        <div className="step-wrapper"></div>
+      </section>
+      <section className="meals">
+        <h3>Discover our tasty meals</h3>
+        <div className="meal-wrapper">
+          <div className="meal-item">
+            {/* <div className="content">
+              <img src={mealImg} alt="chicken and rice" />
+            </div> */}
+            <div className="circle">Chicken & Rice</div>
+          </div>
+        </div>
+      </section>
+      <section className="recipes">
+        <h3>Discover our recipies</h3>
+      </section>
+      <section className="blog">
+        <h3>Stories from our food experts and chef</h3>
+      </section>
+      <section>Contact form</section>
+      <div className="footer">Footer</div>
     </div>
   );
 }
